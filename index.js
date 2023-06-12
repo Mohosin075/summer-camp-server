72fb9425c051c60888f03000da1741c920457a32
Md Alim <mdalimoattabur@gmail.com>
2023-06-12 21:52:48 +0600

    google login user data check and set

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors');
-const port = process.env.POST || 5000
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const port = process.env.POST || 5000;
 
 // MIDDLEWARE
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fudiykq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -17,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -25,30 +25,36 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const classCollection = client.db('summerCamp').collection('classCollections')
-    const userCollection = client.db('summerCamp').collection('userCollection')
+    const classCollection = client.db("summerCamp").collection("classCollections");
+    const userCollection = client.db("summerCamp").collection("userCollection");
 
     // users
-    app.post('/users', async(req, res)=>{
-        const user = req.body
-        console.log(user);
-        const result = await userCollection.insertOne(user)
-        res.send(result)
-      })
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = {email : user.email}
+      const existing = await userCollection.findOne(query)
+      if(existing){
+        return res.send({message : 'already insert'})
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
     // class collection
-    app.get('/classes', async(req, res)=>{
-        const query = {status : 'approved'}
-        const result= await classCollection.find(query).toArray()
-        if(!result){
-            res.status(401).send({error : true, message : 'not found'})
-        }
-        res.send(result)
-    })
+    app.get("/classes", async (req, res) => {
+      const query = { status: "approved" };
+      const result = await classCollection.find(query).toArray();
+      if (!result) {
+        res.status(401).send({ error: true, message: "not found" });
+      }
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -56,13 +62,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("summer camp is running");
+});
 
-
-app.get('/', (req, res)=>{
-    res.send('summer camp is running')
-})
-
-
-app.listen(port, ()=>{
-    console.log(`summer camp is runnig on port : ${port}`);
-})
+app.listen(port, () => {
+  console.log(`summer camp is runnig on port : ${port}`);
+});
