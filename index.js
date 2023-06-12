b468685664fbc43497f8db6faf9145d4ecdcd77d
Md Alim <mdalimoattabur@gmail.com>
2023-06-12 14:46:26 +0600

    create user collection and get class data from mongodb

--- index.js
+++ index.js
@@ -24,6 +24,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const classCollection = client.db('summerCamp').collection('classCollections')
+
+
+    // class collection
+    app.get('/classes', async(req, res)=>{
+        const result= await classCollection.find().toArray()
+        if(!result){
+            res.status(401).send({error : true, message : 'not found'})
+        }
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
